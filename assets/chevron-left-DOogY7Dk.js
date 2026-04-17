962006b2e06cabd8fe444c5d4227befcb47f307e
asura <deviseo@kakao.com>
2026-04-17 17:09:37 +0700

    Updates

--- assets/chevron-left-DOogY7Dk.js
+++
@@ -1,6 +0,0 @@
-import{c as e}from"./TheLayout.vue_vue_type_script_setup_true_lang-DoE6VF1Y.js";/**
- * @license lucide-vue-next v0.469.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const t=e("ChevronLeftIcon",[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]]);export{t as C};
---
+++ assets/supabase-BO6-y7hU.js
@@ -0,0 +1,1 @@
+import{c as s}from"./supabase-mNyy4ix0.js";const a="https://marsnvwgqwjakvogzhbk.supabase.co",e="sb_publishable_E_3vgCGkTDOJw16a2dU_DQ_xZXzalbS",o=s(a,e);export{o as s};
